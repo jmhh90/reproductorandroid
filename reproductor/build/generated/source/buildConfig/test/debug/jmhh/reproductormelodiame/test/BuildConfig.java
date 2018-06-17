aee20748bdf18ed7e242401c2a87b9e13270332e
Jose <you@example.com>
2018-06-17 12:10:48 +0200

    fix consumer

--- reproductor/build/generated/source/buildConfig/test/debug/jmhh/reproductormelodiame/test/BuildConfig.java
+++ reproductor/build/generated/source/buildConfig/test/debug/jmhh/reproductormelodiame/test/BuildConfig.java
@@ -5,7 +5,7 @@ package jmhh.reproductormelodiame.test;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "IdAPPFacebook";
+  public static final String APPLICATION_ID = "jmhh.reproductormelodiame.test";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = -1;
